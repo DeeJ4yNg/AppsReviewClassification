aa70629dfb32d8ff12a78d7758c0413eec9bc10b
DeeJ4yNg <99658722+DeeJ4yNg@users.noreply.github.com>
2022-05-22 11:24:53 +0800

    Update Main_Tiktok.py

--- Main_Tiktok.py
+++ Main_Tiktok.py
@@ -1,7 +1,3 @@
-#Date: Apr 15
-#Modified: djwu
-#Ref: http://www.mccormickml.com/
-#Title: Bert pretrained model test on cola public dataset
 from Dataset_Tiktok import Data_Sets
 from Train_val_Tiktok import Train_Validate
 import warnings
@@ -14,8 +10,8 @@ learning_rate = 0.00001
 adam_epsilon = 1e-8
 epochs = 5
 path_to_save = "./model_save/"
-Vocab_Path = r'C:\Users\djwu\Documents\bert_lite\vocab.txt'
-Bert_Path = r'C:\Users\djwu\Documents\bert_lite'
+Vocab_Path = "./vocab.txt"
+Bert_Path = "./distilbert"
 
 #Training set and Validation set
 train_dataloader, validation_dataloader, tokenizer = Data_Sets(data_path, batch_size, Vocab_Path)
